39d74b8dd8e804a9b831493113c02d1989a54817
cong <zhicong.tay@gmail.com>
2019-11-11 20:40:35 +0800

    Demo commit 'remove mail capitalize, rect done registration to compartments page'

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/compartments';
 
     /**
      * Create a new controller instance.
